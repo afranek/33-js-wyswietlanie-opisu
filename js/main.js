5342fcfcae7817e9b47cdddc2f416bac93d6af6b
afranek <128502600+afranek@users.noreply.github.com>
2023-03-26 14:54:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let desc = document.getElementById("opis");
+let desc2 = document.querySelector("button");
+
+desc2.onclick = function() {
+    desc.textContent = "Najlepsze słuchawki na rynku!"
+}
+
